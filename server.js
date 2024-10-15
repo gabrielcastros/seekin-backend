75e6f6c2ce48aacf4ecd67468899aa5db04e3934
Gabriel <gabrielsoaresdecastro@gmail.com>
2024-10-15 16:44:38 -0300

    Alteração no app.listen()

--- server.js
+++ server.js
@@ -63,6 +63,7 @@ app.get(`/findPlaylistWithTrack`, async (req, res) => {
     }
 })
 
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
